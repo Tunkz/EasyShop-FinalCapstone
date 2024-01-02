2fac012b61f0ed6e934048dc70eab912e7e88072
Tunkz <Bubatunkz00@gmail.com>
2024-01-02 08:55:42 -0500

    Finished ShoppingCartController class

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -2,9 +2,7 @@ package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -20,22 +18,18 @@ import java.security.Principal;
 @RestController
 @CrossOrigin
 @RequestMapping("/cart")
-public class ShoppingCartController
-{
+public class ShoppingCartController {
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
 
 
-
     // each method in this controller requires a Principal object as a parameter
     @RequestMapping("/products")
     @PreAuthorize("hasRole('ROLE_USER') or ahsRole('ROLE_ADMIN')")
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
+    public ShoppingCart getCart(Principal principal) {
+        try {
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
@@ -44,23 +38,53 @@ public class ShoppingCartController
 
             // use the shoppingcartDao to get all items in the cart and return the cart
             return shoppingCartDao.getByUserId(userId);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
-
+    @PostMapping("/products/{productId}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public ShoppingCart addProductToCart(Principal principal, @PathVariable int productId) {
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+            return shoppingCartDao.getByUserId(userId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
-
+    @PutMapping("/products/{productId}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public ShoppingCart updateProductInCart(Principal principal, @PathVariable int productId) {
+        try {
+            String username = principal.getName();
+            User user = userDao.getByUserName(username);
+            int userId = user.getId();
+            return shoppingCartDao.getByUserId(userId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
-
+    @DeleteMapping("/cart")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public void clearCart(Principal principal){
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
